f190a193c024dc0ef0e3892b0da8d90561d6a6a7
msimpson <madison_simpson@outlook.com>
2024-02-26 15:57:21 -0500

    50% complete. Moving from work to home

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-
+from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -12,7 +13,47 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
-
+def start_timer():
+    count_down(300)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    minute = math.floor(count / 60)
+    seconds = count % 60
+    canvas.itemconfig(timer_text, text=f"{minute}:{seconds}")
+    print(count)
+    if count > 0:
+
+        window.after(1000, count_down, count-1)
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+
+tomato_img = PhotoImage(file="tomato.png")
+
+canvas.create_image(100, 112, image=tomato_img)
+
+# Row 0
+title_label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 50), bg=YELLOW)
+title_label.grid(column=1, row=0)
+
+# Row 1
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+# Row 2
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+# Row 3
+check_marks = Label(text="✓", fg=GREEN, bg=YELLOW)
+check_marks.grid(column=1, row=3)
+
+
+window.mainloop()
